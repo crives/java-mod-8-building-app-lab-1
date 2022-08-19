1a5f00ac1f557b346dff462ea1e9f3e7a1326e01
Caroline Rives <car.rives@gmail.com>
2022-08-19 15:59:02 -0400

    Building Lab 3

--- src/app/contact-list-component/contact-list-component.component.ts
+++ src/app/contact-list-component/contact-list-component.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactListComponentComponent implements OnInit {
 
+  users = [
+    { firstName: "Aurelie" },
+    { firstName: "James" },
+    { firstName: "Jessica" },
+    { firstName: "Ludovic" },
+    { firstName: "Maria" },
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/conversation-control-component/conversation-control-component.component.ts
+++ src/app/conversation-control-component/conversation-control-component.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConversationControlComponentComponent implements OnInit {
 
+  conversations = [
+    {
+      id: 1,
+      users: [
+        { firstName: "Claire" },
+        { firstName: "Ludovic" },
+        { firstName: "Jessica" },
+      ],
+    },
+    {
+      id: 2,
+      users: [{ firstName: "Claire" }, { firstName: "James" }],
+    },
+    {
+      id: 3,
+      users: [
+        { firstName: "Claire" },
+        { firstName: "Aurelie" },
+        { firstName: "James" },
+        { firstName: "Jessica" },
+      ],
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
