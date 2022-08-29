d93789dc7562088255b2b311399ac2ca03ac23ef
Caroline Rives <car.rives@gmail.com>
2022-08-29 00:12:26 -0400

    Add BE spring boot files

---
+++ spring-boot-angular-http/src/main/java/com/example/messageapp/model/Message.java
@@ -0,0 +1,15 @@
+package com.example.messageapp.model;
+
+public class Message {
+    public User sender;
+    public String text;
+    public int conversationId;
+    public int sequenceNumber;
+
+    public Message(User sender, String text, int conversationId, int sequenceNumber){
+        this.sender = sender;
+        this.text = text;
+        this.conversationId = conversationId;
+        this.sequenceNumber = sequenceNumber;
+    }
+}
